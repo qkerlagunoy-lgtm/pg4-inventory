202cc389957830015102ec587a9f8907b1ba554b
qkerlagunoy-lgtm <qkerlagunoy@tip.edu.ph>
2026-01-21 08:04:14 +0800

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function Dashboard()
+    {
+        if(Auth::check() && Auth::user()->type == "user"){
+            return view('dashboard');
+        }
+        else if(Auth::check() && Auth::user()->type == "admin"){
+            return view('admin.dashboard');
+        } 
+        else{
+            return redirect()->route('login');
+        }
+    }
+}
---
+++ database/migrations/2026_01_20_044935_add_type_to_users.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('type')->default('user');
+            
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            if (Schema::hasColumn('users', 'type')) {
+                $table->dropColumn('type');
+            }
+        });
+    }
+};
