28eea2cae8b305122dd8d73199e03309ca5fd7a7
AMIIRRR <qaejlee@tip.edu.ph>
2026-01-23 09:25:34 +0800

    save changes

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.landing');
-})->name('landing');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
