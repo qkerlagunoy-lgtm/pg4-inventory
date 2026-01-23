c1f3593a22fc054d9357a0538ef89526fcc35816
KENNETHIRVIN <qkiaduldulao@tip.edu.ph>
2026-01-23 10:37:53 +0800

    Resolve merge conflicts in routes and migrations

--- database/migrations/2026_01_22_022901_create_request_items_table.php
+++ database/migrations/2026_01_22_022901_create_request_items_table.php
@@ -6,20 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('items', function (Blueprint $table) {
+        Schema::create('request_items', function (Blueprint $table) {
             $table->id();
-            $table->string('category');
-            $table->string('item_name');
-            $table->text('description')->nullable();
-            $table->integer('available_quantity')->default(0);
+            $table->foreignId('item_request_id')->constrained()->onDelete('cascade');
+            $table->foreignId('item_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('items');
+        Schema::dropIfExists('request_items');
     }
 };
